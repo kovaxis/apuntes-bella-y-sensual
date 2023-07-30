a485cc77982fac9f5cf4875f984f85b6885ab111
kovaxis <martin.andrigheti@gmail.com>
2023-07-29 23:21:23 -0400

    add primality and minkowski

---
+++ geo2d/minkowski.cpp
@@ -0,0 +1,22 @@
+void reorder_polygon(vector<P> &ps) {
+    int pos = 0;
+    repx(i, 1, (int)ps.size()) {
+        if (ps[i].y < ps[pos].y || (ps[i].y == ps[pos].y && ps[i].x < ps[pos].x))
+            pos = i;
+    }
+    rotate(ps.begin(), ps.begin() + pos, ps.end());
+}
+vector<P> minkowski(vector<P> ps, vector<P> qs) {
+    // the first vertex must be the lowest
+    reorder_polygon(ps); reorder_polygon(qs);
+    ps.push_back(ps[0]); ps.push_back(ps[1]);
+    qs.push_back(qs[0]); qs.push_back(qs[1]);
+    vector<P> result; int i = 0, j = 0;
+    while (i < ps.size() - 2 || j < qs.size() - 2) {
+        result.push_back(ps[i] + qs[j]);
+        auto z = (ps[i + 1] - ps[i]) / (qs[j + 1] - qs[j]);
+        if (z >= 0 && i < ps.size() - 2) ++i;
+        if (z <= 0 && j < qs.size() - 2) ++j;
+    }
+    return result;
+}
