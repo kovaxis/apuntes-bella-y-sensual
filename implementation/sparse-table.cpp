0ffcbd895bffb8aa8ca67819ea740e134e505f49
kovaxis <martin.andrigheti@gmail.com>
2023-08-07 12:39:48 -0300

    refactor sparse table

--- implementation/sparse-table.cpp
+++ implementation/sparse-table.cpp
@@ -1,23 +1,22 @@
 // handle immutable range maximum queries (or any idempotent query) in O(1)
 template <class T>
 struct Sparse {
-    vector<vector<T>> st;
-
     T op(T a, T b) { return max(a, b); }
 
+    vector<vector<T>> st;
     Sparse() {}
+    Sparse(int N) : st{vector<T>(N)} {}
 
-    void reset(int N) { st = {vector<T>(N)}; }
-    void set(int i, T val) { st[0][i] = val; }
+    T &operator[](int i) { return st[0][i]; }
 
-    // O(N log N) time
-    // O(N log N) memory
+    // O(N log N) time, O(N log N) memory
     void init() {
         int N = st[0].size();
         int npot = N <= 1 ? 1 : 32 - __builtin_clz(N);
         st.resize(npot);
         repx(i, 1, npot) rep(j, N + 1 - (1 << i)) st[i].push_back(
-            op(st[i - 1][j], st[i - 1][j + (1 << (i - 1))])); // query op
+                op(st[i - 1][j], st[i - 1][j + (1 << (i - 1))])
+        ); // query op
     }
 
     // query maximum in the range [l, r) in O(1) time
