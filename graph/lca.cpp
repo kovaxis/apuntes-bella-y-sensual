3059456ef9cb0971a498f98a2d5750dc2fa352bf
kovaxis <martin.andrigheti@gmail.com>
2023-08-11 10:35:07 -0300

    actualizar lca

--- graph/lca.cpp
+++ graph/lca.cpp
@@ -1,41 +1,34 @@
 // calculates the lowest common ancestor for any two nodes in O(log N) time,
 // with O(N log N) preprocessing
 struct Lca {
-    int L;
-    vector<vector<int>> up;
-    vector<pair<int, int>> time;
+    int N, K, t = 0;
+    vector<vector<int>> U;
+    vector<int> L, R;
 
     Lca() {}
-    void init(const vector<vector<int>> &G) {
-        int N = G.size(); L = N <= 1 ? 0 : 32 - __builtin_clz(N - 1);
-        up.resize(L + 1); rep(l, L + 1) up[l].resize(N);
-        time.resize(N); int t = 0; visit(G, 0, 0, t);
-        rep(l, L) rep(i, N) up[l + 1][i] = up[l][up[l][i]];
+    Lca(vector<vector<int>> &G) : N(G.size()), L(N), R(N) {
+        K = N <= 1 ? 0 : 32 - __builtin_clz(N - 1);
+        U.resize(K + 1, vector<int>(N));
+        visit(G, 0, 0);
+        rep(k, K) rep(u, N) U[k + 1][u] = U[k][U[k][u]];
     }
 
-    void visit(const vector<vector<int>> &G, int i, int p, int &t) {
-        up[0][i] = p;
-        time[i].first = t++;
-        for (int edge : G[i]) {
-            if (edge == p) continue;
-            visit(G, edge, i, t);
-        }
-        time[i].second = t++;
+    void visit(vector<vector<int>> &G, int u, int p) {
+        L[u] = t++, U[0][u] = p;
+        for (int v : G[u]) if (v != p) visit(G, v, u);
+        R[u] = t++;
     }
 
     bool is_anc(int up, int dn) {
-        return time[up].first <= time[dn].first &&
-               time[dn].second <= time[up].second;
+        return L[up] <= L[dn] && R[dn] <= R[up];
     }
 
-    int get(int i, int j) {
-        if (is_anc(i, j)) return i;
-        if (is_anc(j, i)) return j;
-        int l = L;
-        while (l >= 0) {
-            if (is_anc(up[l][i], j)) l--;
-            else i = up[l][i];
-        }
-        return up[0][i];
+    int find(int u, int v) {
+        if (is_anc(u, v)) return u;
+        if (is_anc(v, u)) return v;
+        for (int k = K; k >= 0;)
+            if (is_anc(U[k][u], v)) k--;
+            else u = U[k][u];
+        return U[0][u];
     }
 };
