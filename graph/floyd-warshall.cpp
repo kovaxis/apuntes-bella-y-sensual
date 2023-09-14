783f795b9272c88ac0525da0283e6bda54a3e9f5
kovaxis <martin.andrighetti@gmail.com>
2023-09-14 00:01:36 -0300

    reformat floyd warshall

--- graph/floyd-warshall.cpp
+++ graph/floyd-warshall.cpp
@@ -1,10 +1,10 @@
-// O(V^3) time and O(V^2) memory.
-// requires an NxN array to store results.
+// calculate distances between every pair of nodes in O(V^3) time.
 // works with negative edges, but not negative cycles.
-void floyd(const vector<vector<pair<int, ll>>> &G, vector<vector<ll>> &dists) {
+void floyd(const vector<vector<pair<ll, int>>> &G, vector<vector<ll>> &D) {
     int N = G.size();
-    rep(i, N) rep(j, N) dists[i][j] = i == j ? 0 : INF;
-    rep(i, N) for (auto edge : G[i]) dists[i][edge.first] = edge.second;
-    rep(k, N) rep(i, N) rep(j, N)
-        dists[i][j] = min(dists[i][j], dists[i][k] + dists[k][j]);
+    D.assign(N, vector<ll>(N, INF));
+    rep(u, N) D[u][u] = 0;
+    rep(u, N) for (auto [w, v] : G[u]) D[u][v] = w;
+    rep(k, N) rep(u, N) rep(v, N)
+        D[u][v] = min(D[u][v], D[u][k] + D[k][v]);
 }
