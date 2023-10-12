3569874a5911aec9cabc531b92a8af27bb297892
Ignacio Muñoz <ignaciomunoz@uc.cl>
2023-10-12 16:25:46 -0300

    Add bit tricks

---
+++ implementation/bit-tricks.cpp
@@ -0,0 +1,19 @@
+y = x & (x-1)  // Turn off rightmost 1bit
+y = x & (-x)   // Isolate rightmost 1bit
+y = x | (x-1)  // Right propagate rightmost 1bit(fill in 1s)
+y = x | (x+1)  // Turn on rightmost 0bit
+y = ~x & (x+1) // Isolate rightmost 0bit
+// If x is of long type, use __builtin_popcountl(x)
+// If x is of long long type, use __builtin_popcountll(x)
+// 1. Counts the number of one’s(set bits) in an integer.
+__builtin_popcount(x)
+// 2. Checks the Parity of a number. Returns true(1) if the
+// number has odd number of set bits, else it returns
+// false(0) for even number of set bits.
+ __builtin_parity(x) 
+// 3. Counts the leading number of zeros of the integer.
+__builtin_clz(x)
+// 4. Counts the trailing number of zeros of the integer.
+__builtin_ctz(x)
+// 5. Returns 1 + the index of the least significant 1-bit.
+__builtin_ffs(x) // If x == 0, returns 0.
