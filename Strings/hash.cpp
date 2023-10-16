fd6a546399ac19466458c132ab4250c2cf6b1684
Ignacio Muñoz <ignaciomunoz@uc.cl>
2023-10-16 02:25:07 -0300

    New hash

--- Strings/hash.cpp
+++ Strings/hash.cpp
@@ -1,60 +1,24 @@
-// compute substring hashes in O(1).
-// hashes are compatible between different strings.
-struct Hash {
-    ll HMOD;
-    int N;
-    vector<int> h;
-    vector<int> p;
-
-    Hash() {}
-    // O(N)
-    Hash(const string &s, ll HMOD_ = 1000003931)
-        : N(s.size() + 1), HMOD(HMOD_), p(N), h(N) {
-        static const ll P =
-            chrono::steady_clock::now().time_since_epoch().count() % (1 << 29);
-        p[0] = 1;
-        rep(i, N - 1) p[i + 1] = p[i] * P % HMOD;
-        rep(i, N - 1) h[i + 1] = (h[i] + (ll)s[i] * p[i]) % HMOD;
+struct Hash{
+    const int K = 2;
+    const ll MOD[K] = {999727999, 1070777777};
+    const ll P = 1777771;
+    vector<ll> h[K], p[K];
+    Hash(string &s){
+        int n = s.size();
+        rep(k, K){
+            h[k].resize(n+1, 0);
+            p[k].resize(n+1, 1);
+            repx(i, 1, n+1){
+                h[k][i] = (h[k][i-1]*P + s[i-1]) % MOD[k];
+                p[k][i] = (p[k][i-1]*P) % MOD[k];
+            }
+        }
     }
-
-    // O(1)
-    pair<ll, int> get(int i, int j) { return {(h[j] - h[i] + HMOD) % HMOD, i}; }
-
-    bool cmp(pair<ll, int> x0, pair<ll, int> x1) {
-        int d = x0.second - x1.second;
-        ll &lo = d < 0 ? x0.first : x1.first;
-        lo = lo * p[abs(d)] % HMOD;
-        return x0.first == x1.first;
-    }
-};
-
-// compute hashes in multiple prime modulos simultaneously,
-// to reduce the chance of collisions.
-struct HashM {
-    int N;
-    vector<Hash> sub;
-
-    HashM() {}
-    // O(K N)
-    HashM(const string &s, const vector<ll> &mods) : N(mods.size()), sub(N) {
-        rep(i, N) sub[i] = Hash(s, mods[i]);
-    }
-
-    // O(K)
-    vector<pair<ll, int>> get(int i, int j) {
-        vector<pair<ll, int>> hs(N);
-        rep(k, N) hs[k] = sub[k].get(i, j);
-        return hs;
-    }
-
-    bool cmp(const vector<pair<ll, int>> &x0, const vector<pair<ll, int>> &x1) {
-        rep(i, N) if (!sub[i].cmp(x0[i], x1[i])) return false;
-        return true;
-    }
-
-    bool cmp(int i0, int j0, int i1, int j1) {
-        rep(i, N) if (!sub[i].cmp(sub[i].get(i0, j0),
-                                  sub[i].get(i1, j1))) return false;
-        return true;
+    vector<ll> get(int i, int j){
+        vector<ll> r(K);
+        rep(k, K){
+            r[k] = (h[k][j] - h[k][i]*p[k][j-i]) % MOD[k];
+            r[k] = (r[k] + MOD[k]) % MOD[k];
+        } return r;
     }
 };
