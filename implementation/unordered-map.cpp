1a3f82eb310cf3b8b75a9002e4fb134263296fce
kovaxis <martin.andrighetti@gmail.com>
2023-10-18 17:53:55 -0300

    add rnd(a, b) function

--- implementation/unordered-map.cpp
+++ implementation/unordered-map.cpp
@@ -1,30 +1,15 @@
-// hackproof rng
 static mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rnd(a,b) (uniform_int_distribution<ll>(a,b)(rng))
 
-// deterministic rng
-uint64_t splitmix64(uint64_t *x) {
-    uint64_t z = (*x += 0x9e3779b97f4a7c15);
-    z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
-    z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
-    return z ^ (z >> 31);
-}
-
-// hackproof unordered map hash
 struct Hash {
     size_t operator()(const ll &x) const {
-        static const uint64_t RAND =
-            chrono::steady_clock::now().time_since_epoch().count();
+        static const uint64_t RAND = chrono::steady_clock
+            ::now().time_since_epoch().count();
         uint64_t z = x + RAND + 0x9e3779b97f4a7c15;
         z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
         z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
         return z ^ (z >> 31);
     }
 };
-
-// hackproof unordered_map
-template <class T, class U>
-using umap = unordered_map<T, U, Hash>;
-
-// hackproof unordered_set
-template <class T>
-using uset = unordered_set<T, Hash>;
+template<class T,class U>using umap=unordered_map<T,U,Hash>;
+template<class T> using uset = unordered_set<T, Hash>;
