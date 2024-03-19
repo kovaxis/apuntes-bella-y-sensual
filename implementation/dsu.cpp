ae5e24d23aff130d1d9609521f0ce85e1582f515
kovaxis <martin.andrighetti@gmail.com>
2024-03-19 13:24:07 -0300

    compress dsu

--- implementation/dsu.cpp
+++ implementation/dsu.cpp
@@ -1,11 +1,12 @@
 struct Dsu {
-    vector<int> p; Dsu() {} Dsu(int N) : p(N, -1) {}
+    vector<int> p; Dsu(int N = 0) : p(N, -1) {}
     int get(int x) { return p[x] < 0 ? x : get(p[x]); }
     bool sameSet(int a, int b) { return get(a) == get(b); }
     int size(int x) { return -p[get(x)]; }
     vector<vector<int>> S;
     void unite(int x, int y) {
-        if ((x = get(x)) == (y = get(y))) { S.push_back({-1}); return; }
+        if ((x = get(x)) == (y = get(y)))
+            return S.push_back({-1});
         if (p[x] > p[y]) swap(x, y);
         S.push_back({x, y, p[x], p[y]});
         p[x] += p[y], p[y] = x;
