59865b07c772a32639ab115fb0f2044fc970344e
kovaxis <martin.andrighetti@gmail.com>
2024-04-09 17:14:59 -0400

    remove binexp from arithmetic

--- math/arithmetic.cpp
+++ math/arithmetic.cpp
@@ -6,13 +6,3 @@ inline ll floordiv(ll a, ll b) {
 inline ll ceildiv(ll a, ll b) {
     return a / b + ((a ^ b) >= 0 && a % b);
 }
-
-ll binexp(ll a, ll e) {
-    ll res = 1; // neutral element
-    while (e) {
-        if (e & 1) res = res * a; // multiplication
-        a = a * a;                // multiplication
-        e >>= 1;
-    }
-    return res;
-}
