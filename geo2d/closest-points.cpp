1d80c0870d14dfc4fdecb321d28564b379468248
MrNachoX <ignaciomunoz@uc.cl>
2024-04-09 17:24:34 -0400

    Add closest points dp

---
+++ geo2d/closest-points.cpp
@@ -0,0 +1,21 @@
+// sort by x
+ll closest(vector<ii> &p) {
+    int n = SZ(p);
+    set<ii> s;
+    ll best = 1e18;
+    int j = 0;
+    fore(i, 0, n) {
+        ll d = ceil(sqrt(best));
+        while(p[i].fst - p[j].fst >= best)
+            s.erase({p[j].snd, p[j].fst}), j++;
+        auto it1=s.lower_bound({p[i].snd-d,p[i].fst});
+        auto it2=s.upper_bound({p[i].snd+d,p[i].fst});
+        for(auto it = it1; it != it2; ++it) {
+            ll dx = p[i].fst - it->snd;
+            ll dy = p[i].snd - it->fst;
+            best = min(best, dx * dx + dy * dy);
+        }
+        s.insert({p[i].snd, p[i].fst});
+    }
+    return best;
+}
