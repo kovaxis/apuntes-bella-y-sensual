c60b0364b21db83e64b90883035dac6678ce21ef
MrNachoX <ignaciomunoz@uc.cl>
2024-04-10 10:31:59 -0400

    Add treap

--- data structures/Treap.cpp
+++ data structures/Treap.cpp
@@ -1,136 +1,30 @@
-mt19937 gen(chrono::high_resolution_clock::now().time_since_epoch().count());
-
-//  101 Implicit Treap  //
-
-struct Node {
-    int p, sz = 0, v, acc, l = -1, r = -1;
-    Node() : v(0), acc(0) {}
-    Node(int x) : p(gen()), sz(1), v(x), acc(x) {}
-    void recalc(const Node &a, const Node &b) {
-        sz = a.sz + b.sz + 1;
-        acc = v + a.acc + b.acc;
-    }
+typedef struct item *pitem;
+struct item {
+	int pr,key,cnt; pitem l,r;
+	item(int key):key(key),pr(rand()),cnt(1),l(0),r(0) {}
 };
-
-template <class node>
-struct Treap {
-    vector<node> t;
-    int n, r = -1;
-
-    node get(int u) { return u != -1 ? t[u] : node(); }
-    void recalc(int u) { t[u].recalc(get(t[u].l), get(t[u].r)); }
-    int merge(int l, int r) {
-        if (min(l, r) == -1) return l != -1 ? l : r;
-        int ans = (t[l].p < t[r].p) ? l : r;
-        if (ans == l) t[l].r = merge(t[l].r, r), recalc(l);
-        if (ans == r) t[r].l = merge(l, t[r].l), recalc(r);
-        return ans;
-    }
-    pii split(int u, int id) {
-        if (u == -1) return {-1, -1};
-        int szl = get(t[u].l).sz;
-        if (szl >= id) {
-            pii ans = split(t[u].l, id);
-            t[u].l = ans.ss;
-            recalc(u);
-            return {ans.ff, u};
-        }
-        pii ans = split(t[u].r, id - szl - 1);
-        t[u].r = ans.ff;
-        recalc(u);
-        return {u, ans.ss};
-    }
-
-    Treap(vi &v) : n(sz(v)) {
-        for (int i = 0; i < n; i++) t.eb(v[i]), r = merge(r, i);
-    }
-};
-
-//  Complete Implicit Treap with Lazy propagation  //
-
-struct Node {
-    int p, sz = 0, v, acc, l = -1, r = -1, par = -1, lzv = 0;
-    bool lz = false, f = false;
-    Node() : v(0), acc(0) {}
-    Node(int x) : p(gen()), sz(1), v(x), acc(x) {}
-    void recalc(const Node &a, const Node &b) {
-        sz = a.sz + b.sz + 1;
-        acc = v + a.acc + b.acc;
-    }
-    void upd_lazy(int x) { lz = 1, lzv += x; }
-    void lazy() { v += lzv, acc += sz * lzv, lz = 0, lzv = 0; }
-    void flip() { swap(l, r), f = 0; }
-};
-
-template <class node>
-struct Treap {
-    vector<node> t;
-    int n, r = -1;
-
-    node get(int u) { return u != -1 ? t[u] : node(); }
-    void recalc(int u) {
-        int l = t[u].l, r = t[u].r;
-        push(l), push(r), flip(l), flip(r);
-        t[u].recalc(get(l), get(r));
-    }
-    void push(int u) {
-        if (u == -1 || !t[u].lz) return;
-        int l = t[u].l, r = t[u].r;
-        if (l != -1) t[l].upd_lazy(t[u].lzv);
-        if (r != -1) t[r].upd_lazy(t[u].lzv);
-        t[u].lazy();
-    }
-    void flip(int u) {
-        if (u == -1 || !t[u].f) return;
-        int l = t[u].l, r = t[u].r;
-        if (l != -1) t[l].f ^= 1;
-        if (r != -1) t[r].f ^= 1;
-        t[u].flip();
-    }
-    int merge(int l, int r) {
-        if (min(l, r) == -1) return l != -1 ? l : r;
-        push(l), push(r), flip(l), flip(r);
-        int ans = (t[l].p < t[r].p) ? l : r;
-        if (ans == l) t[l].r = merge(t[l].r, r), recalc(l);
-        if (ans == r) t[r].l = merge(l, t[r].l), recalc(r);
-        if (t[ans].l != -1) t[t[ans].l].par = ans; // only if parent needed
-        if (t[ans].r != -1) t[t[ans].r].par = ans; // only if parent needed
-        return ans;
-    }
-    pii split(int u, int id) {
-        if (u == -1) return {-1, -1};
-        push(u);
-        flip(u);
-        int szl = get(t[u].l).sz;
-        if (szl >= id) {
-            pii ans = split(t[u].l, id);
-            if (ans.ss != -1) t[ans.ss].par = u;  // only if parent needed
-            if (ans.ff != -1) t[ans.ff].par = -1; // only if parent needed
-            t[u].l = ans.ss;
-            recalc(u);
-            return {ans.ff, u};
-        }
-        pii ans = split(t[u].r, id - szl - 1);
-        if (ans.ff != -1) t[ans.ff].par = u;  // only if parent needed
-        if (ans.ss != -1) t[ans.ss].par = -1; // only if parent needed
-        t[u].r = ans.ff;
-        recalc(u);
-        return {u, ans.ss};
-    }
-    int update(int u, int l, int r, int v) {
-        pii a = split(u, l), b = split(a.ss, r - l + 1);
-        t[b.ff].upd_lazy(v);
-        return merge(a.ff, merge(b.ff, b.ss));
-    }
-    void print(int u) {
-        if (u == -1) return;
-        push(u), flip(u);
-        print(t[u].l);
-        cout << t[u].v << ' ';
-        print(t[u].r);
-    }
-
-    Treap(vi &v) : n(sz(v)) {
-        for (int i = 0; i < n; i++) t.eb(v[i]), r = merge(r, i);
-    }
-};
+int cnt(pitem t){return t?t->cnt:0;}
+void upd_cnt(pitem t){if(t)t->cnt=cnt(t->l)+cnt(t->r)+1;}
+void split(pitem t, int key, pitem& l, pitem& r){ // l: <= key, r: > key
+	if(!t)l=r=0;
+	else if(key<t->key)split(t->l,key,l,t->l),r=t;
+	else split(t->r,key,t->r,r),l=t;
+	upd_cnt(t);
+}
+void insert(pitem& t, pitem it){
+	if(!t)t=it;
+	else if(it->pr>t->pr)split(t,it->key,it->l,it->r),t=it;
+	else insert(it->key<t->key?t->l:t->r,it);
+	upd_cnt(t);
+}
+void merge(pitem& t, pitem l, pitem r){
+	if(!l||!r)t=l?l:r;
+	else if(l->pr>r->pr)merge(l->r,l->r,r),t=l;
+	else merge(r->l,l,r->l),t=r;
+	upd_cnt(t);
+}
+void erase(pitem& t, int key){
+	if(t->key==key)merge(t,t->l,t->r);
+	else erase(key<t->key?t->l:t->r,key);
+	upd_cnt(t);
+}
---
+++ data structures/treap-implicit.cpp
@@ -0,0 +1,120 @@
+mt19937 gen(chrono::high_resolution_clock::now().time_since_epoch().count());
+//  101 Implicit Treap  //
+struct Node {
+    int p, sz = 0, v, acc, l = -1, r = -1;
+    Node() : v(0), acc(0) {}
+    Node(int x) : p(gen()), sz(1), v(x), acc(x) {}
+    void recalc(const Node &a, const Node &b) {
+        sz = a.sz + b.sz + 1; acc = v + a.acc + b.acc;
+    }
+};
+template <class node>
+struct Treap {
+    vector<node> t;
+    int n, r = -1;
+    node get(int u) { return u != -1 ? t[u] : node(); }
+    void recalc(int u){t[u].recalc(get(t[u].l),get(t[u].r));}
+    int merge(int l, int r) {
+        if (min(l, r) == -1) return l != -1 ? l : r;
+        int ans = (t[l].p < t[r].p) ? l : r;
+        if (ans == l) t[l].r = merge(t[l].r, r), recalc(l);
+        if (ans == r) t[r].l = merge(l, t[r].l), recalc(r);
+        return ans;
+    }
+    pii split(int u, int id) {
+        if (u == -1) return {-1, -1};
+        int szl = get(t[u].l).sz;
+        if (szl >= id) {
+            pii ans = split(t[u].l, id);
+            t[u].l = ans.ss;
+            recalc(u);
+            return {ans.ff, u};
+        }
+        pii ans = split(t[u].r, id - szl - 1);
+        t[u].r = ans.ff;
+        recalc(u);
+        return {u, ans.ss};
+    }
+    Treap(vi &v) : n(sz(v)) {
+        for (int i=0; i<n; i++) t.eb(v[i]), r = merge(r, i);
+    }
+};
+//  Complete Implicit Treap with Lazy propagation  //
+struct Node {
+    int p, sz = 0, v, acc, l = -1, r = -1, par = -1, lzv=0;
+    bool lz = false, f = false;
+    Node() : v(0), acc(0) {}
+    Node(int x) : p(gen()), sz(1), v(x), acc(x) {}
+    void recalc(const Node &a, const Node &b) {
+        sz = a.sz + b.sz + 1; acc = v + a.acc + b.acc;
+    }
+    void upd_lazy(int x) { lz = 1, lzv += x; }
+    void lazy() {v+=lzv, acc += sz * lzv, lz = 0, lzv = 0; }
+    void flip() { swap(l, r), f = 0; }
+};
+template <class node> struct Treap {
+    vector<node> t;
+    int n, r = -1;
+    node get(int u) { return u != -1 ? t[u] : node(); }
+    void recalc(int u) {
+        int l = t[u].l, r = t[u].r;
+        push(l), push(r), flip(l), flip(r);
+        t[u].recalc(get(l), get(r));
+    }
+    void push(int u) {
+        if (u == -1 || !t[u].lz) return;
+        int l = t[u].l, r = t[u].r;
+        if (l != -1) t[l].upd_lazy(t[u].lzv);
+        if (r != -1) t[r].upd_lazy(t[u].lzv);
+        t[u].lazy();
+    }
+    void flip(int u) {
+        if (u == -1 || !t[u].f) return;
+        int l = t[u].l, r = t[u].r;
+        if (l != -1) t[l].f ^= 1;
+        if (r != -1) t[r].f ^= 1;
+        t[u].flip();
+    }
+    int merge(int l, int r) { // (*) = only if parent needed
+        if (min(l, r) == -1) return l != -1 ? l : r;
+        push(l), push(r), flip(l), flip(r);
+        int ans = (t[l].p < t[r].p) ? l : r;
+        if (ans == l) t[l].r = merge(t[l].r, r), recalc(l);
+        if (ans == r) t[r].l = merge(l, t[r].l), recalc(r);
+        if (t[ans].l != -1) t[t[ans].l].par = ans; // (*)
+        if (t[ans].r != -1) t[t[ans].r].par = ans; // (*)
+        return ans;
+    }
+    pii split(int u, int id) {// (*) = only if parent needed
+        if (u == -1) return {-1, -1};
+        push(u);
+        flip(u);
+        int szl = get(t[u].l).sz;
+        if (szl >= id) {
+            pii ans = split(t[u].l, id);
+            if (ans.ss != -1) t[ans.ss].par = u;  // (*)
+            if (ans.ff != -1) t[ans.ff].par = -1; // (*)
+            t[u].l = ans.ss;
+            recalc(u);
+            return {ans.ff, u};
+        }
+        pii ans = split(t[u].r, id - szl - 1);
+        if (ans.ff != -1) t[ans.ff].par = u;  // (*)
+        if (ans.ss != -1) t[ans.ss].par = -1; // (*)
+        t[u].r = ans.ff;
+        recalc(u);
+        return {u, ans.ss};
+    }
+    int update(int u, int l, int r, int v) {
+        pii a = split(u, l), b = split(a.ss, r - l + 1);
+        t[b.ff].upd_lazy(v);
+        return merge(a.ff, merge(b.ff, b.ss));
+    }
+    void print(int u) {
+        if (u==-1) return; push(u), flip(u); print(t[u].l);
+        cout << t[u].v << ' '; print(t[u].r);
+    }
+    Treap(vi &v) : n(sz(v)) {
+        for (int i=0; i<n; i++) t.eb(v[i]), r = merge(r, i);
+    }
+};
