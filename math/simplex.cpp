ce584f4520bfd49d829e463200f40f56433c5fb6
MrNachoX <ignaciomunoz@uc.cl>
2024-04-10 11:16:16 -0400

    compress

--- math/simplex.cpp
+++ math/simplex.cpp
@@ -1,44 +1,34 @@
-// Solves a general linear maximization problem: maximize $c^T x$ subject to $Ax \le b$, $x \ge 0$.
-// Returns -inf if there is no solution, inf if there are arbitrarily good solutions, or the maximum value of $c^T x$ otherwise.
-// The input vector is set to an optimal $x$ (or in the unbounded case, an arbitrary solution fulfilling the constraints).
-// Numerical stability is not guaranteed. For better performance, define variables such that $x = 0$ is viable.
-// Usage:
-// vvd A = {{1,-1}, {-1,1}, {-1,-2}};
-// vd b = {1,1,-4}, c = {-1,-1}, x;
-// T val = LPSolver(A, b, c).solve(x);
-// Time: O(NM * \#pivots), where a pivot may be e.g. an edge relaxation. O(2^n) in the general case.
-
-#include "../common.h"
-
-typedef double T; // long double, Rational, double + mod<P>...
+/* Solves a general linear maximization problem: maximize
+$c^T x$ subject to $Ax \le b$, $x \ge 0$. Returns -inf if
+there is no solution, inf if there are arbitrarily good
+solutions, or the maximum value of $c^T x$ otherwise. The
+input vector is set to an optimal $x$ (or in the unbounded
+case, an arbitrary solution fulfilling the constraints).
+Numerical stability is not guaranteed. For better
+performance, define variables such that $x = 0$ is viable.
+Usage:
+vvd A = {{1,-1}, {-1,1}, {-1,-2}};
+vd b = {1,1,-4}, c = {-1,-1}, x;
+T val = LPSolver(A, b, c).solve(x);
+Time: O(NM * \#pivots), where a pivot may be e.g. an edge
+relaxation. O(2^n) in the general case.*/
+typedef double T;//long double, Rational, double + mod<P>...
 typedef vector<T> vd;
 typedef vector<vd> vvd;
-
 const T eps = 1e-8, inf = 1 / .0;
 #define MP make_pair
 #define ltj(X) \
     if (s == -1 || MP(X[j], N[j]) < MP(X[s], N[s])) s = j
-
 struct LPSolver {
-    int m, n;
-    vector<int> N, B;
-    vvd D;
-
-    LPSolver(const vvd &A, const vd &b, const vd &c) : m(b.size()), n(c.size()), N(n + 1), B(m), D(m + 2, vd(n + 2)) {
+    int m, n; vector<int> N, B; vvd D;
+    LPSolver(const vvd &A,const vd &b,const vd &c) : m(b.size()),n(c.size()),N(n+1),B(m),D(m+2,vd(n+2)){
         rep(i, m) rep(j, n) D[i][j] = A[i][j];
         rep(i, m) {
-            B[i] = n + i;
-            D[i][n] = -1;
-            D[i][n + 1] = b[i];
+            B[i] = n + i; D[i][n] = -1; D[i][n + 1] = b[i];
         }
-        rep(j, n) {
-            N[j] = j;
-            D[m][j] = -c[j];
-        }
-        N[n] = -1;
-        D[m + 1][n] = 1;
+        rep(j, n) { N[j] = j; D[m][j] = -c[j]; }
+        N[n] = -1; D[m + 1][n] = 1;
     }
-
     void pivot(int r, int s) {
         T *a = D[r].data(), inv = 1 / a[s];
         rep(i, m + 2) if (i != r && abs(D[i][s]) > eps) {
@@ -51,7 +41,6 @@ struct LPSolver {
         D[r][s] = inv;
         swap(B[r], N[s]);
     }
-
     bool simplex(int phase) {
         int x = m + phase - 1;
         for (;;) {
@@ -67,7 +56,6 @@ struct LPSolver {
             pivot(r, s);
         }
     }
-
     T solve(vd &x) {
         int r = 0;
         repx(i, 1, m) if (D[i][n + 1] < D[r][n + 1]) r = i;
