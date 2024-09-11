a594c49f0a378dc9c8767d99e068028750f6e28f
kovaxis <martin.andrighetti@gmail.com>
2024-09-11 03:50:30 -0300

    actualizar treap

--- data structures/treap.cpp
+++ data structures/treap.cpp
@@ -1,30 +1,30 @@
-typedef struct item *pitem;
-struct item {
-    int pr,key,cnt; pitem l,r;
-    item(int key):key(key),pr(rand()),cnt(1),l(0),r(0) {}
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+struct item { // to allow duplicates, add random val to keys
+    int p, k, n; item *l=0, *r=0;
+    item(int k) : p(rng()), k(k), n(1) {}
 };
-int cnt(pitem t){return t?t->cnt:0;}
-void upd_cnt(pitem t){if(t)t->cnt=cnt(t->l)+cnt(t->r)+1;}
-void split(pitem t, int key, pitem& l, pitem& r){ // l: <= key, r: > key
-    if(!t)l=r=0;
-    else if(key<t->key)split(t->l,key,l,t->l),r=t;
-    else split(t->r,key,t->r,r),l=t;
-    upd_cnt(t);
+int getn(item *x) { return x ? x->n : 0; }
+void recalc(item* x) { if(x) x->n=getn(x->l)+1+getn(x->r); }
+void split(item *x, int k, item *&l, item *&r) {
+    if (!x) l = r = 0;
+    else if (k < x->k) split(x->l, k, l, x->l), r = x;
+    else split(x->r, k, x->r, r), l = x;
+    recalc(x);
 }
-void insert(pitem& t, pitem it){
-    if(!t)t=it;
-    else if(it->pr>t->pr)split(t,it->key,it->l,it->r),t=it;
-    else insert(it->key<t->key?t->l:t->r,it);
-    upd_cnt(t);
+void merge(item *&x, item *l, item *r) {
+    if (!l || !r) x = l ? l : r;
+    else if (l->p > r->p) merge(l->r, l->r, r), x = l;
+    else merge(r->l, l, r->l), x = r;
+    recalc(x);
 }
-void merge(pitem& t, pitem l, pitem r){
-    if(!l||!r)t=l?l:r;
-    else if(l->pr>r->pr)merge(l->r,l->r,r),t=l;
-    else merge(r->l,l,r->l),t=r;
-    upd_cnt(t);
+void insert(item *&x, item *y) {
+    if (!x) x = y;
+    else if (y->p > x->p) split(x, y->k, y->l, y->r), x = y;
+    else insert(y->k < x->k ? x->l : x->r, y);
+    recalc(x);
 }
-void erase(pitem& t, int key){
-    if(t->key==key)merge(t,t->l,t->r);
-    else erase(key<t->key?t->l:t->r,key);
-    upd_cnt(t);
+void erase(item *&x, int k) {
+    if (x->k == k) merge(x, x->l, x->r);
+    else erase(k < x->k ? x->l : x->r, k);
+    recalc(x);
 }
